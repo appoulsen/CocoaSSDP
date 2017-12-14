83beaaedc0220cf0a079b302ef4d2c11934b4262
Daniel K. Poulsen <daniel.poulsen@cambridgeaudio.com>
2017-12-15 00:25:27 +0100

    added mac support

---
+++ CocoaSSDP-MacOS/CocoaSSDP_MacOS.h
@@ -0,0 +1,19 @@
+//
+//  CocoaSSDP_MacOS.h
+//  CocoaSSDP-MacOS
+//
+//  Created by Daniel K. Poulsen on 15/12/2017.
+//  Copyright © 2017 cambridgeaudio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaSSDP_MacOS.
+FOUNDATION_EXPORT double CocoaSSDP_MacOSVersionNumber;
+
+//! Project version string for CocoaSSDP_MacOS.
+FOUNDATION_EXPORT const unsigned char CocoaSSDP_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaSSDP_MacOS/PublicHeader.h>
+
+
